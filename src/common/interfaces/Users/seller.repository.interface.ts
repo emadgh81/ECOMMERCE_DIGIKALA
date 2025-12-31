8fc94554d3a7686ae4dfb9f4a80db9edddddcb60
Emad Ghanbarian <eghanbarian81@gmail.com>
2025-12-31 21:31:51 +0330

    fix(seller): update seller interface to fix DI issue in ProductService

--- src/common/interfaces/Users/seller.repository.interface.ts
+++ src/common/interfaces/Users/seller.repository.interface.ts
@@ -4,7 +4,7 @@ export const SELLER_REPOSITORY = 'ISellerRepository';
 export interface SellerRepository {
   findAll(): Promise<Seller[]>;
   findById(id: string): Promise<Seller | null>;
-  findByUserId(userId: string): Promise<Seller[]>;
+  findByUserId(userId: string): Promise<Seller | null>;
   createAndSave(seller: Partial<Seller>): Promise<Seller>;
   save(seller: Seller): Promise<Seller>;
   remove(seller: Seller): Promise<void>;
